f85b6e3e4b8ead030391360f71a28e307f765da2
pedrobernardo27 <josepedrobernardo@hotmail.com>
2022-06-03 17:53:56 -0300

    Foram feitas alteraões para corrigir exceptions e colocados controles de fluxo, assim pedindo as informações novamente se foi colocado informações erradas como string no lugar de números, foi tratado também o nulo como não sendo aceitado como um valor.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,27 +6,60 @@ fun main() {
 }
 
 fun calcular() {
-    println("Informe o primeiro número: ")
-    val n1 = readLine()?.toInt()
-    println("Informe a operação: ")
-    val operacao = readLine()
-    println("Informe o segundo número: ")
-    val n2 = readLine()?.toInt()
+    var n1: Int? = null
+    var n2: Int? = null
+    var operacao: String? = null
+    do {
+        println("Informe o primeiro número: ")
+        val retorno = readLine()
+        if (retorno != null && retorno != "") {
+            n1 = retorno.toIntOrNull()
+        } else {
+            println("Valor inválido.")
+        }
+    } while (n1 == null)
+    do {
+        println("Informe a operação: ")
+        val retorno = readLine()
+        if (retorno != null && retorno != "") {
+            if (retorno == "+" || retorno == "-" || retorno == "*" || retorno == "/") {
+                operacao = retorno
+            } else {
+                println("Operação inválida.")
+            }
+        }else{
+            println("Valor inválido.")
+        }
+    } while (operacao == null)
+    do{
+        println("Informe o segundo número: ")
+        val retorno = readLine()
+        if (retorno != null && retorno != "") {
+            n2 = retorno.toIntOrNull()
+        } else {
+            println("Valor inválido.")
+        }
+    }while (n2 == null)
     if (operacao == "+") {
-        val soma = n1!! + n2!!
+        val soma = n1 + n2
         println("O total da soma é de $soma")
     } else if (operacao == "-") {
-        val subtracao = n1!! - n2!!
+        val subtracao = n1 - n2
         println("O total da subtração é $subtracao")
     } else if (operacao == "*") {
-        val multiplicacao = n1!! * n2!!
+        val multiplicacao = n1 * n2
         println("O total da multiplicação é $multiplicacao")
     } else if (operacao == "/") {
-        val divisao = n1!! / n2!!
+        val divisao = n1 / n2
         println("O total da divisão é $divisao")
     }
 
+
 }
+
+
+
+
 
 
 
